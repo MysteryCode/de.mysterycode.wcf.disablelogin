9837b11701b25f872209aa35b23ca172ac449271
mutec <florian@mysterycode.de>
2016-02-17 15:24:53 +0100

    WCF 2.1-API

--- files/lib/system/event/listener/DisableLoginListener.class.php
+++ files/lib/system/event/listener/DisableLoginListener.class.php
@@ -1,24 +1,24 @@
 <?php
 
 namespace wcf\system\event\listener;
-use wcf\system\event\IEventListener;
+use wcf\system\event\listener\IParameterizedEventListener;
 use wcf\system\exception\PermissionDeniedException;
 use wcf\system\WCF;
 
 /**
- * Redirects guests to the login form
+ * Denies access to the login form
  *
  * @author	Florian Gail
- * @copyright	2013 Florian Gail <http://www.mysterycode.de/>
- * @license	Creative Commons <by-nc-nd> <http://creativecommons.org/licenses/by-nc-nd/4.0/legalcode>
+ * @copyright	2013-2016 Florian Gail <http://www.mysterycode.de/>
+ * @license	Kostenlose Plugins <https://downloads.mysterycode.de/license/6-kostenlose-plugins/>
  * @package	de.mysterycode.wcf.disablelogin
  * @category	WCF
  */
-class DisableLoginListener implements IEventListener {
+class DisableLoginListener implements IParameterizedEventListener {
 	/**
-	 * @see \wcf\system\event\IEventListener::execute()
+	 * @see \wcf\system\event\listener\IParameterizedEventListener::execute()
 	 */
-	public function execute($eventObj, $className, $eventName) {
+	public function execute($eventObj, $className, $eventName, array &$parameters) {
 		if (WCF::getUser()->userID == 0)
 			throw new PermissionDeniedException();
 	}
